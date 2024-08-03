ff902ccef45e980a5e5562e7b188e3a8684e8e6c
Artem Titariev <artem.titariev@gmail.com>
2024-08-03 21:42:00 +0300

    [converts] Edit valudation rules, fix migrations

---
+++ app/Http/Requests/StoreConvertRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreConvertRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sql_database.driver' => 'required|string|max:10',
+            // 'sql_database.url' => 'required|string',
+            'sql_database.host' => 'required|string',
+            'sql_database.port' => 'required|integer|gte:0',
+            'sql_database.database' => 'required|string',
+            'sql_database.username' => 'required|string',
+            // 'sql_database.password' => 'required|string',
+            'sql_database.password' => 'nullable|string',
+            'sql_database.charset' => 'required|string|max:10',
+
+            'sql_database.collation' => 'nullable|string',
+            'sql_database.search_path' => 'nullable|string',
+            'sql_database.sslmode' => 'nullable|string',
+
+            
+            'mongo_database.dsn' => 'required|string',
+            'mongo_database.database' => 'required|string',
+
+            'description' => 'required|string',
+        ];
+    }
+}
