f438466aa2ff29cd5931c5ccd746f016ea5064cc
Artem Titariev <artem.titariev@gmail.com>
2024-11-18 21:14:36 +0200

    [refactor] Remove some testing routes

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,11 @@ Route::get('language/{locale}', function ($locale) {
 })->name('lang');
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 })->name('welcome');
 
 Route::get('/dashboard', function () {
-    return view('welcome');
+    return redirect()->route('converts.index');
 })->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -70,8 +70,6 @@ require __DIR__ . '/auth.php';
 
 Route::get('/etl', [TestEtlController::class, 'test']);
 Route::get('/etlo', [TestEtlController::class, 'testOrdinary']);
-
-Route::view('/test', 'test');
 
 Route::get('/delete', function (Request $request) {
     $id = $request->input('id');
@@ -117,41 +115,6 @@ Route::get('/delete-all', function (Request $request) {
     return 'all converts deleted';
 });
 
-// Route::get('/circular', function(Request $request) {
-
-//     $id = $request->input('id');
-//     $convert = Convert::find($id);
-//     $sqlDatabase = $convert->sqlDatabase;
-//     // dd(CircularRef::getByAllTableNames($sqlDatabase->id, ["table1", 'table5']));
-//     dd($sqlDatabase->circularRefs);
-
-//     return 'done';
-// });
-
-Route::get('/result-step4', function (Request $request) {
-
-    $id = $request->input('id');
-    $convert = Convert::find($id);
-
-    $mongoDatabase = $convert->mongoDatabase()
-        ->with(['collections'])
-        ->first();
-
-    $collections = $mongoDatabase->collections()->pluck('id');
-
-    // dd(Field::whereIn('collection_id', [17, 18])->get());
-    dd(
-        ManyToManyLink::whereIn('collection1_id', $collections)
-            ->orWhereIn('collection2_id', $collections)
-            ->orWhereIn('pivot_collection_id', $collections)
-            ->get(),
-
-        LinkEmbedd::whereIn('pk_collection_id', $collections)
-            ->orWhereIn('fk_collection_id', $collections)
-            ->get()
-    );
-});
-
 Route::get('/clear-step4', function (Request $request) {
 
     $id = $request->input('id');
@@ -162,10 +125,6 @@ Route::get('/clear-step4', function (Request $request) {
         ->first();
 
     $collections = $mongoDatabase->collections()->pluck('id');
-
-    // dd($collections);
-    // ->with(['linksEmbeddsFrom', 'linksEmbeddsTo', 'manyToManyPivot'])
-    // ->get();
 
     DB::table('links_embedds')
         ->whereIn('pk_collection_id', $collections)
@@ -195,112 +154,6 @@ Route::get('/reset', function (Request $request) {
     $progress->save();
 
     return 'done';
-});
-
-Route::get('/insert', function () {
-    $id = 1;
-    $convert = Convert::find($id);
-
-    $sqlDatabase = $convert->sqlDatabase;
-    $mongoDatabase = $convert->mongoDatabase;
-
-    $sqlConnection = ConnectionCreator::create($sqlDatabase);
-    $mongoConnection = ConnectionCreator::create($mongoDatabase);
-
-    $collectionName = "posts";
-
-    $mongoConnection->dropCollection($collectionName);
-
-    $record = [
-        "name" => "test",
-    ];
-
-    $doc = [
-        "_id" => "000",
-        "some_key" => 123,
-        'another' => 'fsdfsdfsd',
-    ];
-
-
-    $doc2 = [
-        "_id" => "111",
-        "abc" => 'cba',
-    ];
-
-    // $doc = [
-    //     [
-    //         "some_key" => 123,
-    //         'another' => 'fsdfsdfsd',
-    //     ],
-    //     [
-    //         "some_key" => 555,
-    //         'another' => '---------',
-    //     ],
-    // ];
-
-    $docId = $mongoConnection
-        ->collection($collectionName)
-        ->insertGetId($record);
-
-
-    $mongoConnection->collection($collectionName)->where('_id', $docId)
-        ->push('nested_documents', $doc);
-
-    $mongoConnection->collection($collectionName)->where('_id', '111')
-        ->push('nested', $doc2);
-
-    dd($docId->__toString());
-
-    return 'done';
-});
-
-Route::get('/search', function () {
-    $id = 1;
-    $convert = Convert::find($id);
-
-    $sqlDatabase = $convert->sqlDatabase;
-    $mongoDatabase = $convert->mongoDatabase;
-
-    $sqlConnection = ConnectionCreator::create($sqlDatabase);
-    $mongoConnection = ConnectionCreator::create($mongoDatabase);
-
-    $collectionName = "tags2";
-    $parentId = "672483cc0dac0bd4c30699e9";
-
-    $path = "related.0.nested";
-
-    $parentDoc = $mongoConnection
-        ->collection($collectionName)
-        ->where('_id', $parentId)
-        ->first();
-
-    $nestedArray = data_get($parentDoc, $path) ?? [];
-
-    $mongoConnection
-        ->collection($collectionName)
-        ->where('_id', $parentId)
-        ->push($path, ["key" => 123]);
-
-    $parentDoc = $mongoConnection
-        ->collection($collectionName)
-        ->where('_id', $parentId)
-        ->first();
-
-    dd($nestedArray, $parentDoc);
-});
-
-Route::get('/id-map', function () {
-    dd(DB::table('id_mappings')->get());
-});
-
-Route::get('/id-map-find', function () {
-    $map = DB::table('id_mappings')
-        ->where('table_id', 17)
-        ->where('collection_id', 17)
-        ->whereJsonContains('source_data', ["id" => 1])
-        ->first();
-
-    dd($map);
 });
 
 Route::get('/id-map-del', function () {
